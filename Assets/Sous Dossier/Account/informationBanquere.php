3b30d8fa2577199c98901fc5b16c8e9be9527f48
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-02-01 14:09:42 +0100

    Update Statégique

--- Assets/Sous Dossier/Account/informationBanquere.php
+++ Assets/Sous Dossier/Account/informationBanquere.php
@@ -1,48 +1,57 @@
-<!DOCTYPE html>
-<html lang="fr">
-<title>Connexion</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta charset="UTF-8">
-<link rel="stylesheet" href="../../CSS/AccountGlobale.css">
-
-<body>
-  <form action="informationBanquere.php">
-    <div class="container-register">
-      <h1>adresse</h1>
-      <p>veillez renseigner vos information pour la livraison ci-dessous.</p>
-      <hr>
-
-      <label for="numeroCarte"><b>numéro de la carte</b></label>
-      <input type="number" placeholder="Entrer numéro de la carte" name="numeroCarte" id="PrennumeroCarteom" min="1000000000000000" max="9999999999999999"required>
-
-      <label for="NomCarte"><b>Nom du detenteur</b></label>
-      <input type="text" placeholder="Entrer Nom sur la carte" name="NomCarte" id="NomCarte" required>
-
-      <label for="dateExpi"><b>date d'expiration</b></label>
-      <input type="month" placeholder="Entrer date" name="Adresse" id="Adresse" min="01/01/1900" max="" required>
-      <hr>
-
-      <label for="cryptogramme"><b>cryptogramme de sécurité</b></label>
-      <input type="number" placeholder="Entrer cryptogramme de sécurité" name="cryptogramme" id="cryptogramme">
-      <hr>
-
-      <label for="InfoPay"><b>enregistrer mes informations de payement</b></label>
-      <input type="checkbox" placeholder="enregistrer mes informations de payement" name="InfoPay" id="InfoPay">
-      <hr>
-
-      <button type="submit" class="registerbtn">valider</button>
-    </div>
-  </form>
-</body>
-</html> 
-
 <?php
 session_start();
 include 'BDDConnection.php';
-$bd = bdConnect(); //Connection a la base de donnee
+$bd = bdConnect(); // Connexion à la base de données
+
 if (isset($_SESSION['id'])) {
-  
+  // Code à exécuter si la session 'id' est définie
 } else {
-  (!empty($_POST))
-};
-?>
+  if (!empty($_POST)) {
+    // Utilisation de requêtes préparées pour éviter les injections SQL
+    $stmt = $bd->prepare("INSERT INTO table (numeroCarte, NomCarte, dateExpi, cryptogramme, InfoPay) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $_POST['numeroCarte'], $_POST['NomCarte'], $_POST['dateExpi'], $_POST['cryptogramme'], $_POST['InfoPay']);
+    $stmt->execute();
+  }
+}
+?>  
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Connexion</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../../CSS/AccountGlobale.css">
+  </head>
+  <body>
+    <form action="informationBanquere.php" method="post">
+      <!-- Ajout d'un jeton CSRF pour protéger le formulaire -->
+      <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+      <div class="container-register">
+        <h1>adresse</h1>
+        <p>veuillez renseigner vos informations pour la livraison ci-dessous.</p>
+        <hr>
+
+        <label for="numeroCarte"><b>numéro de la carte</b></label>
+        <input type="number" placeholder="Entrer numéro de la carte" name="numeroCarte" id="PrennumeroCarteom" min="1000000000000000" max="9999999999999999"required>
+
+        <label for="NomCarte"><b>Nom du detenteur</b></label>
+        <input type="text" placeholder="Entrer Nom sur la carte" name="NomCarte" id="NomCarte" required>
+
+        <label for="dateExpi"><b>date d'expiration</b></label>
+        <input type="month" placeholder="Entrer date" name="Adresse" id="Adresse" min="01/01/1900" max="" required>
+        <hr>
+
+        <label for="cryptogramme"><b>cryptogramme de sécurité</b></label>
+        <input type="number" placeholder="Entrer cryptogramme de sécurité" name="cryptogramme" id="cryptogramme">
+        <hr>
+
+        <label for="InfoPay"><b>enregistrer mes informations de payement</b></label>
+        <input type="checkbox" placeholder="enregistrer mes informations de payement" name="InfoPay" id="InfoPay">
+        <hr>
+
+        <button type="submit" class="registerbtn">valider</button>
+      </div>
+    </form>
+  </body>
+</html>
