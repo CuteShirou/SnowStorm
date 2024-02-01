5f95569102bf6a607ab0184b35a041142bb9b187
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-02-01 11:13:32 +0100

    Update fin

--- Assets/Sous Dossier/Account/informationBanquere.php
+++ Assets/Sous Dossier/Account/informationBanquere.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="fr">
-<meta charset="UTF-8">
 <title>Connexion</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
+<meta charset="UTF-8">
 <link rel="stylesheet" href="../../CSS/AccountGlobale.css">
 
 <body>
-  <form action="Register.php">
+  <form action="informationBanquere.php">
     <div class="container-register">
       <h1>adresse</h1>
       <p>veillez renseigner vos information pour la livraison ci-dessous.</p>
       <hr>
 
-      <label for="numeroCarte"><b>numÃ©ro de la carte</b></label>
-      <input type="number" placeholder="Entrer Prenom" name="numeroCarte" id="PrennumeroCarteom" required>
+      <label for="numeroCarte"><b>numéro de la carte</b></label>
+      <input type="number" placeholder="Entrer numéro de la carte" name="numeroCarte" id="PrennumeroCarteom" min="1000000000000000" max="9999999999999999"required>
 
       <label for="NomCarte"><b>Nom du detenteur</b></label>
       <input type="text" placeholder="Entrer Nom sur la carte" name="NomCarte" id="NomCarte" required>
 
-      <label for="Adresse"><b>Adresse</b></label>
+      <label for="dateExpi"><b>date d'expiration</b></label>
       <input type="month" placeholder="Entrer date" name="Adresse" id="Adresse" min="01/01/1900" max="" required>
       <hr>
 
-      <label for="cryptogramme"><b>cryptogramme de sÃ©curitÃ©</b></label>
-      <input type="number" placeholder="Entrer cryptogramme de sÃ©curitÃ©" name="cryptogramme" id="cryptogramme">
+      <label for="cryptogramme"><b>cryptogramme de sécurité</b></label>
+      <input type="number" placeholder="Entrer cryptogramme de sécurité" name="cryptogramme" id="cryptogramme">
       <hr>
 
       <label for="InfoPay"><b>enregistrer mes informations de payement</b></label>
@@ -34,4 +34,15 @@
     </div>
   </form>
 </body>
-</html>
+</html> 
+
+<?php
+session_start();
+include 'BDDConnection.php';
+$bd = bdConnect(); //Connection a la base de donnee
+if (isset($_SESSION['id'])) {
+  
+} else {
+  (!empty($_POST))
+};
+?>
